de4c237c79e318e104bf46ce4a634f003b5919ba
Fathor Rahman <fathorrahman2357@gmail.com>
2023-10-05 13:04:00 +0700

    update: add firestore

--- lib/app/modules/add_property/controllers/add_property_controller.dart
+++ lib/app/modules/add_property/controllers/add_property_controller.dart
@@ -4,15 +4,49 @@ import 'package:get/get.dart';
 
 class AddPropertyController extends GetxController {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
-  final propertyTitle = TextEditingController();
-  final propertyLocation = TextEditingController();
-  final propertySurface = TextEditingController();
+
+  late TextEditingController propertyTitle;
+  late TextEditingController propertyLocation;
+  late TextEditingController propertySurface;
+
+  // add property to firebase
+  void addProperty(String title, String location, String surface) async {
+    CollectionReference property = firestore.collection("properties");
+    try {
+      await property.add({
+        "title": title,
+        "location": location,
+        "surface": surface,
+      });
+
+      Get.defaultDialog(
+        title: "Success",
+        middleText: "Property was added",
+        onConfirm: () => Get.back(),
+        textConfirm: "OKAY",
+      );
+    } catch (e) {
+      print(e);
+      Get.defaultDialog(
+        title: "Error!",
+        middleText: "Error to save property",
+      );
+    }
+  }
 
   @override
   void onInit() {
+    propertyTitle = TextEditingController();
+    propertyLocation = TextEditingController();
+    propertySurface = TextEditingController();
     super.onInit();
   }
 
-  // add property to firebase
-  void addProperty(String title, String location, String surface) async {}
+  @override
+  void onClose() {
+    propertyTitle.dispose();
+    propertyLocation.dispose();
+    propertySurface.dispose();
+    super.onClose();
+  }
 }
